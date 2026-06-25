623951ad293c4da343d8a428cfa297123f06418d
Sophia Grace <sopgra@ceh.ac.uk>
2026-06-25 17:14:55 +0100

    Statistical identification of flood start, peak and end functions

--- scripts/my_floodanalysis_utils.py
+++ scripts/my_floodanalysis_utils.py
@@ -130,3 +130,151 @@ def catchment_mean_rainfall(
     # = a 1-D array of length T (the number of time steps), containing the mean rainfall over the catchment at each time step.
 
     
+# ----- Statistically identifying the flood event (start, peak and end) from a data set: ----
+# Trying to write code/ functions to compute this:
+
+# Calculating the variance across a moving window to identify where there is a sudden
+# increase in variance, and therefore where there might be the start of a flood event
+# this returns a variance series which is put into the next function along
+def moving_variance(Q, window):
+    return Q.rolling(window= window, min_periods = 1).var()
+
+# Determine the flood threshold (fth) over which the variance must exceed for it to be counted as a 
+# flood event, rather than just background flood flow
+# the threshold in fischer et al., 2021 is defined as the mean of the moving variances + 0.25 *
+# the standard deviation of these variances
+def compute_fth(var_series, std_var_multiple = 2.5):
+    mean_var = var_series.mean()
+    std_var = var_series.std()
+    return mean_var + std_var_multiple * std_var # threshold raised to 2.5 * std_var, so it only detects stronger variability signals
+
+
+# Identify the flood event by taking in a series/ column from a dataframe containing the discharge flow
+def find_events(Q, dvar=7, peak_flow_multiple = 5, time_slice_multiple = 10, end_idx_multiple = 0.15, min_time_slices = 5, flood_closeness_time_slices = 5): #dvar = 5 = stops the variance reacting too much to tiny wiggles
+    
+    """
+    Q : discharge flow series
+    dvar : ___
+    peak_flow_multiple : defines how many time slices into the future, relative to the length of the ascending arm,
+    should be searched for the peak threshold. Default is 5 * the length of the ascending arm
+    time_slice_multiple : defines how short a flood rising event is before it is discarded as too short to be a true
+    flood event. Default is 10 time slices (corresponding to discharge increaes for durations of 150 minutes in 15 min
+    temporal resolution data). This should vary depending on the temporal resolution of the data.
+    end_idx_multiple : defines how low the discharge volume should be, relative to the peak flow, to help determine the end
+    of the flood event (one of two criteria). Default is 0.15 (i.e. flow should be 15% of the peak flow) but this can be varied
+    depending on the catchment
+    min_time_slices : defines how many time slices long theevent must be to be discarded as not a real event. Here, default is 5,
+    so with a 15 min temporal resolution dataset flood events with rising limbs shorter than 75min long aren't counted as true
+    flood events, to prevent false identification of many flood events
+    flood_closeness_time_slices : if more than one peak is identified, this defines how much time can pass before they are treated
+    as separate events, or until they are merged. The default here is 5 time slices, so if less than 75 mins (5 * 15 min res) passes 
+    between two separate flood peaks, they are merged into the same flood event. 
+    """
+    # Convert the flow into a series
+    Q = pd.Series(Q).reset_index(drop=True) 
+    Q = Q.rolling(window = 3, center= True).mean().bfill().ffill()
+    # This smooths the discharge right after converting to a series, to remove any noise nad stop variance from flickering too much
+
+    # Lag-1 differences
+    QD = Q.diff() # Produces a new DF calld QD (i.e chanage in discharge) which contains the
+                    # difference between each row and the row before in the discharge df
+    QD.iloc[0] = 0 # This sets the very first difference as 0 (as there has been no diff yet)
+
+    # Moving variance
+    var_series = moving_variance(Q, dvar) # returns a variance series containing the moving variances
+
+    # Threshold
+    threshold = compute_fth(var_series) # coputing the threshold based on this variance series
+
+    # Active points
+    active = var_series > threshold # identifies where the flood events start, as per our criteria
+                                    # returning a series (of same length as the discharge series)
+                                    # of bool values (True = active event! False = not an event)
+    events = [] # sets up an empty list to store results, where results will each be stored as a dictionary
+    i = 0 # start index = 0
+    n = len(Q) # n is the length of the event's dataframe (i.e. the total number of timesteps)
+
+    # this whole looping logic is looping through the whole timeseries and extracting one flood event at a time
+    while i < n: # while we haven't cycled though the whole event yet...
+        # Detect if we are inside a flood event:
+        if active.iloc[i] and QD.iloc[i] > 0:# and Q.iloc[i] > 0.3 * Q.max(): # Look at the current row/ index in the 'active' series. 
+                            # if this index is True, it'll enter this loop. If it isn't True,
+                            # it will move onto the next time slice and see whether that one is True
+            # Find start
+            start = i # initial index for the start of the event. This is shifted backwards in time by this:
+            # flow_threshold = Q.quantile(0.25)
+            while start > 0 and QD.iloc[start] >0 : # while we aren't at the beginning of the dataset                                                 # AND the flow is still increasing ...
+                start -= 1 #... hop back to the previous row to check whether this is still increasing.
+                # this loop is expandingthe event backwards through time to capture the full rising limb
+                # = overall, this only allows forward movement through time if the variance stays above the threshold    
+                # it will stop increasing once the flow no longer increases, or once we reach teh start of the dataset
+            
+            # Ignore events after main peak decay
+            if start > Q.idxmax() + 200:
+                i += 1
+                continue
+
+            end_temp = i # Find temporary end OF THE HIGH VARIANCE REGION (which we take as what was the initial start)
+            while end_temp < n and active.iloc[end_temp]: # while we are still between the rising limb and 
+                                                        # and the temporary end 
+                end_temp += 1
+
+            # Identify the Peak of the flow:
+            search_end = min(end_temp + int(peak_flow_multiple * (end_temp - start)), n)
+
+            peak_idx = Q.iloc[start:search_end].idxmax()  # find the maximum discharge value
+            # ... calculate the max discharge volume in the rising limb
+            # this sums the volume of water across the rising limb = ~ the sum discharge between the start to the peak
+            # ("search for a peak value within a region where we are confident there is something "event like" happening")
+            # (this isn't even the full event, but rather just the active core of the event that we are looking at currently)
+            if (peak_idx - start) < time_slice_multiple: # if this rising limb is too short (i.e. shorter than 150 minutes = ~ 2hours), ignore the event and move
+                i += 1
+                continue
+
+            # -- only real events should reach this point -- #
+
+            # End using volume balance
+            rising_vol = Q.iloc[start:peak_idx].sum() # Calculate the volume of water discharged between the start of the event and the event peak
+            cumulative = 0
+            end = peak_idx # this is a safe default initialisation of the end index. i.e. “If we can’t find a better end, at least we have the peak”
+
+            # Identifying the end of the event
+            for j in range(peak_idx + 1, n): # start at the peak's index & move foward through time for the duration of the rest of the series
+                cumulative += Q.iloc[j] # add that flow value to the cumulative (i.e. falling limb integral)
+                if cumulative >= rising_vol and Q.iloc[j] < end_idx_multiple * Q.iloc[peak_idx]:# Repeat this until the descending limb vol equals or exceeds the rising limb vol...
+                                                                                    #AND until the discharge is less than 15% of the peak flow.
+                    end = j # then identify at which index this value is achieved...
+                    break # and end the loop
+
+            if (end - start) >= min_time_slices: # this removes events that are smaller than 5 * temp res (i.e. 75 min long), which are fake events probably
+                events.append({ # add the indices to the events list of dictionaries
+                "start": start,
+                "peak": peak_idx,
+                "end": end
+            })
+
+            i = end + 1 # Once the start, peak and end are found, it doesn't go onto the next row from the start row,
+                        # but instead jumps straight to the index after the end of the event, so it can't double/ triple etc. count the same event!
+        else:
+            i += 1 # and if the current point isn't active, move onto the next one to check that one. 
+        
+        events_df = pd.DataFrame(events)
+        
+        # Events are merged post-processing to convert many tiny flood signals into one comprehensive flood signal
+        if len(events_df) > 1:
+            merged = [events_df.iloc[0].to_dict()] # this is the first detected event...
+
+            for _, row in events_df.iloc[1:].iterrows(): #each subsequent flood event is then looked at...
+                prev = merged[-1] # the current event and previous merged event are compared
+
+                # the time gap between the events is checked. If the gap is small enough, they are close enough to be the same flood event
+                if row['start'] - prev['end'] <= flood_closeness_time_slices:
+                    prev['end'] = row['end'] # this extends the end of the event to cover both events
+                    prev['peak'] = max(prev['peak'], row['peak']) # this updates the peak so the peak is still the highest value
+                else:
+                    merged.append(row.to_dict()) # but they are kept as separate events if they aren't close to each other. 
+
+            events_df = pd.DataFrame(merged)
+
+
+    return events_df
